1c79096fe9dd694d8117cc719404c9d25ec984a1
Aidi Stan <aidistan@live.com>
2017-05-20 18:43:33 +0800

    Update Account page
    - Import user's gender from Weixin user info
    - And allow User to update his gender

--- app/pages/account.js
+++ app/pages/account.js
@@ -4,16 +4,21 @@ Page({
   data: {
     loaded: false,
 
+    // Remote data
     user: {},
     userInfo: {},
-    addresses: []
+    addresses: [],
+
+    // User-derived data
+    GENDERS: ['男', '女'],
+    genderIndex: 0
   },
   onLoad: function () {
     app.loginWeixin(() => {
       app.getUserInfo((userInfo) => this.setData({userInfo: userInfo}))
 
       app.loginQingDiet(() => {
-        app.getUser((user) => this.setData({user: user}))
+        app.getUser((user) => this.updateUser(user))
         app._getAddresses((addresses) => this.setData({addresses: addresses}))
 
         this.setData({loaded: true})
@@ -25,9 +30,20 @@ Page({
       app.loginQingDiet(() => app._getAddresses((addresses) => this.setData({addresses: addresses})))
     }
   },
+  bindGenderChange: function (e) {
+    app._updateUser({
+      gender: e.detail.value === '0' ? 'male' : 'female'
+    }, (user) => this.updateUser(user))
+  },
   bindBirthdayChange: function (e) {
     app._updateUser({
       birthday: e.detail.value
-    }, (user) => this.setData({user: user}))
+    }, (user) => this.updateUser(user))
+  },
+  updateUser: function (user) {
+    this.setData({
+      user: user,
+      genderIndex: user.gender === 'female' ? 1 : 0
+    })
   }
 })
