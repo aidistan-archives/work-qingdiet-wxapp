be8cc429e46800416216b0f756a0f8ad32ceb67a
Aidi Stan <aidistan@live.com>
2017-05-21 15:12:39 +0800

    Store dependencies in app/vendor

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp')
 var exec = require('child_process').exec
 
+gulp.task('vendor', function () {
+  gulp.src('node_modules/weui-wxss/dist/style/weui.wxss')
+    .pipe(gulp.dest('app/vendor'))
+  gulp.src('node_modules/async/dist/async.js')
+    .pipe(gulp.dest('app/vendor'))
+})
+
 gulp.task('watch', function () {
   gulp.watch('app/**/*.js', function (event) {
     exec('node_modules/.bin/standard --fix ' + event.path)
